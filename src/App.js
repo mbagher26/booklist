23f085478f79216d3b9b9ecac59a2b6caefcd6b5
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-03-13 06:18:25 -0400

    import Header and AddForm component in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import AddForm from './components/BookList/AddForm';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
- 
+      <Header/>
+      <AddForm/>
     </div>
   );
 }
