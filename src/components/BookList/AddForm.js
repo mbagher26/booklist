ff4458c2415e7ca7301e89f646ddc4e3c1eacb98
mbagher26 <mohammadbaghermollanory@gmail.com>
2024-03-13 06:08:44 -0400

    create value and onChange event in dom for AddForm component

--- src/components/BookList/AddForm.js
+++ src/components/BookList/AddForm.js
@@ -27,17 +27,17 @@ export default class AddForm extends Component {
                 <form id="book-form" autocomplete="off" onChange={(event)=>this.submitHandler(event)}>
                     <div className="form-group">
                         <label htmlFor="title">Title</label>
-                        <input type="text" id="title" className="form-control" />
+                        <input type="text" id="title" value={this.state.title} onChange={event => this.titleHandler(event)} className="form-control" />
                     </div>
 
                     <div className="form-group">
                         <label htmlFor="author">Author</label>
-                        <input type="text" id="author" className="form-control" />
+                        <input type="text" id="author" value={this.state.author} onChange={event => this.authorHandler(event)} className="form-control" />
                     </div>
 
                     <div className="form-group">
                         <label htmlFor="year">Year</label>
-                        <input type="text" id="year" className="form-control" />
+                        <input type="text" id="year" value={this.state.year} onChange={event => this.yearHandler(event)} className="form-control" />
                     </div>
                     <input type="submit" value="Add Book" className="btn btn-warning btn-block add-btn" />
                 </form>
